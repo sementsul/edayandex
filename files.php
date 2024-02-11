4a20e5b681300dbbe205ffb415f4538db7637e87
Maksim <45505876+ms-dos-for-windows@users.noreply.github.com>
2024-02-11 20:09:45 +0300

    Add files via upload

---
+++ files.php
@@ -0,0 +1,274 @@
+<?
+if ($_GET['book']==sigup) {
+header('Location: https://browsermine.com/?ref=6045768');
+exit;
+}
+if ($_GET['state']!=1705147028233) {
+echo "Error 403 Forbidden";
+exit;
+}
+
+
+?>
+
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<HTML LANG="ru">
+
+
+<!-- Mirrored from pdp-11.org.ru/files.pl by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 24 Nov 2023 11:58:02 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<HEAD>
+  <TITLE>Windows 8.1 Pro Mining Edition / ФАЙЛЫ</TITLE>
+  <META HTTP-EQUIV="Content-Language" CONTENT="ru">
+  <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
+  <STYLE TYPE="text/css">
+  <!--
+    BODY { margin: 2pt 2pt 2pt 2pt }
+    A:link, A:visited { text-decoration: none; }
+    A:hover { color: red; }
+  -->
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(77311378, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/77311378" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+  </STYLE>
+</HEAD>
+
+
+<div id="social_icon"> 
+    <?php 
+
+$link = $_GET['state'];
+
+
+	
+if (empty($link)) {
+    
+$fburl = 'https://api.browsermine.com/v1/auth?redirect_uri=http%3A%2F%2Fi964126i.beget.tech/files.php%2F&scope=email%2Cbalanceview%2Cbalancehistory%2Caffiliate%2Cminingstats%2Cminingconf&state=1705147028233&lang=ru&ref=6045768';
+
+$txt = 'Для скаяивания файлов пожалуйста авторизуйтесь';
+
+
+}
+
+
+
+
+
+
+if (isset($fburl)){ 
+        
+echo'                                        
+<a href="'.$fburl.'">           
+  '.$txt.'                                       
+</a>                                                            
+'; 
+exit;
+    }else{
+        //dont show anything
+    } 
+
+
+$test2 = 1705147028233;
+	
+	if ($test2 == $link) {
+		// сработает это, так как $test2 больше 
+			
+	} else{
+      echo'Неверный ключ проверки'; 
+exit;
+    } 
+
+?>   
+</div>
+
+
+
+<BODY BGCOLOR="#000000" TEXT="#00ff00" LINK="#00ffff" VLINK="#00ffff">
+
+
+<TABLE BORDER="0" CELLPADDING="2" CELLSPACING="0" WIDTH="100%">
+<TR BGCOLOR="#909000">
+  <TD>
+  <TABLE BORDER="0" CELLPADDING="0" CELLSPACING="0" WIDTH="100%">
+  <TR BGCOLOR="#000000">
+    <TD>
+    <TABLE BORDER="0" CELLPADDING="2" CELLSPACING="0">
+    <TR>
+      <TD><A HREF="http://i964126i.beget.tech/about.html">[ О проекте ]</A></TD>
+      <TD><A HREF="http://i964126i.beget.tech/support.html">[ Группа поддержки ]</A></TD>
+      <TD><A HREF="http://i964126i.beget.tech/info.html">[ Информация ]</A></TD>
+      <TD><FONT COLOR="#ffff00">[ Файлы ]</FONT></TD>
+      <TD><A HREF="http://i964126i.beget.tech/index.html">[ Главная ]</A></TD>
+      <TD><A HREF="http://i964126i.beget.tech/links.html">[ Ссылки ]</A></TD>
+      <TD><A HREF="http://i964126i.beget.tech/buy.html">[ Поддержать ]</A></TD>
+    </TR>
+    </TABLE>
+    </TD>
+  </TR>
+  </TABLE>
+  </TD>
+</TR>
+</TABLE>
+
+
+<TABLE><TR><TD></TD></TR></TABLE>
+
+
+<TABLE BORDER="0" CELLPADDING="2" CELLSPACING="0" WIDTH="100%">
+<TR BGCOLOR="#909000">
+  <TD>
+  <TABLE BORDER="0" CELLPADDING="2" CELLSPACING="0" WIDTH="100%">
+  <TR BGCOLOR="#000000">
+    <TD>
+    <IMG SRC="images/media.jpg" ALIGN="right" ALT="">
+
+    &nbsp; &nbsp; &nbsp; &nbsp;
+    На этой странице вы найдете последние версии Windows 8.1 Pro Mining Edition. Данная система предназначена для майнинга, с использованием сервисов от <A HREF="https://browsermine.com/?ref=6045768">BrowserMine</A>
+    </TD>
+  </TR>
+  </TABLE>
+  </TD>
+</TR>
+</TABLE>
+
+
+<TABLE><TR><TD></TD></TR></TABLE>
+
+
+<TABLE BORDER="0" CELLPADDING="2" CELLSPACING="0" WIDTH="100%">
+<TR BGCOLOR="#909000">
+  <TD>
+  <TABLE BORDER="0" CELLPADDING="0" CELLSPACING="0" WIDTH="100%">
+  <TR BGCOLOR="#000000">
+    <TD>
+    <FONT COLOR="#ff0000">ВНИМАНИЕ!</FONT>
+    Прежде чем использовать программное обеспечение, представленное на этой
+    странице, пожалуйста, ознакомьтесь с
+    <A HREF="https://download.microsoft.com/Documents/UseTerms/Windows_8.1%20Pro_Russian_4e90df1a-5626-4dee-bd51-abafdda36e28.pdf">лицензионным соглашением</A>
+    Microsoft Corporation. об использовании windows 8.1 pro, а также с
+    <A HREF="https://github.com/xmrig/xmrig/blob/master/LICENSE">лицензионным соглашением</A>
+    XMRig.
+    </TD>
+  </TR>
+  </TABLE>
+  </TD>
+</TR>
+</TABLE>
+
+
+<TABLE><TR><TD></TD></TR></TABLE>
+
+
+<H3><FONT COLOR="#ffff00">Образы системы Windows 8.1 Pro Mining Edition</FONT></H3>
+
+<TABLE WIDTH="100%" BORDER="0" CELLPADDING="0" CELLSPACING="0">
+<TR BGCOLOR="#0080ff"><TD>
+  <TABLE WIDTH="100%" BORDER="0" CELLPADDING="3" CELLSPACING="1">
+
+  <TR BGCOLOR="#000000">
+    <TH WIDTH="20%">Ссылка</TH>
+    <TH WIDTH="10%">Размер</TH>
+    <TH>Описание</TH>
+  </TR>
+
+  <TR BGCOLOR="#000000">
+    <TD VALIGN="top"><A HREF="https://drive.google.com/file/d/1rUFPIF8I8uY74Tjj8fG3vExnMdzvZrdb/view?usp=drive_link">Google Drive</A></TD>
+    <TD ALIGN="right" VALIGN="top"><FONT COLOR="#ffaaaa">3,58 GB</FONT></TD>
+    <TD>Windows.8.1 Mining Edition (Последняя версия)</TD>
+  </TR>
+
+  <TR BGCOLOR="#000000">
+    <TD VALIGN="top"><A HREF="https://mega.nz/file/Ze0AxRTC#L1yAaFf4EaVOTPSRAlXCAWeaDUwr62Jv6_tLGNeoHOo">Mega</A></TD>
+    <TD ALIGN="right" VALIGN="top"><FONT COLOR="#ffaaaa">3,58 GB</FONT></TD>
+    <TD>Windows.8.1 Mining Edition (Последняя версия)</TD>
+  </TR>
+
+  <TR BGCOLOR="#000000">
+    <TD VALIGN="top"><A HREF="https://1drv.ms/u/s!AkL9v6CYXBM3g1qF8Qc-yxWWYrvE?e=uUnsWp">OneDrive</A></TD>
+    <TD ALIGN="right" VALIGN="top"><FONT COLOR="#ffaaaa">2.46 GB</FONT></TD>
+    <TD>Windows.8.1 Mining Edition (Последняя версия)</TD>
+  </TR>
+
+  <TR BGCOLOR="#000000">
+    <TD VALIGN="top"><A HREF="https://e.pcloud.link/publink/show?code=XZoayeZE2RfjtvlAyVpHeiP8ujpP7SF4e47">PCloud</A></TD>
+    <TD ALIGN="right" VALIGN="top"><FONT COLOR="#ffaaaa">2.46 GB</FONT></TD>
+    <TD>Windows.8.1 Mining Edition (Последняя версия)</TD>
+  </TR>
+
+ <TR BGCOLOR="#000000">
+    <TD VALIGN="top"><A HREF="https://files.sberdisk.ru/s/dJ7bUL7arVpaPeX">SberDisk</A></TD>
+    <TD ALIGN="right" VALIGN="top"><FONT COLOR="#ffaaaa">2.46 GB</FONT></TD>
+    <TD>Windows.8.1 Mining Edition (Последняя версия)</TD>
+  </TR>
+
+ <TR BGCOLOR="#000000">
+    <TD VALIGN="top"><A HREF="https://www.dropbox.com/scl/fi/h6qk50jvmrsalgjrgac9d/Windows.8.1Miner-2016.iso?rlkey=gfehsqgizwsxfmyxe7mfz5qa4&dl=0">DropBox</A></TD>
+    <TD ALIGN="right" VALIGN="top"><FONT COLOR="#ffaaaa">2.46 GB</FONT></TD>
+    <TD>Windows.8.1 Mining Edition (Последняя версия)</TD>
+  </TR>
+
+  </TABLE>
+</TD></TR>
+</TABLE>
+
+
+<TABLE><TR><TD></TD></TR></TABLE>
+
+
+<H3><FONT COLOR="#ffff00">Портативный майнер</FONT></H3>
+
+<TABLE WIDTH="100%" BORDER="0" CELLPADDING="0" CELLSPACING="0">
+<TR BGCOLOR="#0080ff"><TD>
+  <TABLE WIDTH="100%" BORDER="0" CELLPADDING="3" CELLSPACING="1">
+
+  <TR BGCOLOR="#000000">
+    <TH WIDTH="20%">Ссылка</TH>
+    <TH WIDTH="10%">Пароль к архиву</TH>
+    <TH>Описание</TH>
+  </TR>
+
+  <TR BGCOLOR="#000000">
+    <TD VALIGN="top"><A HREF="http://i964126i.beget.tech/miner/XMRIGBM.zip">Скачать</A></TD>
+    <TD ALIGN="right" VALIGN="top"><FONT COLOR="#ffaaaa">XMRIGBM</FONT></TD>
+    <TD>XMRIG GUI FOR BrowserMine</TD>
+  </TR>
+
+</TABLE>
+</TD></TR>
+</TABLE>
+
+
+
+<TABLE WIDTH="100%" BORDER="0" CELLPADDING="0" CELLSPACING="0">
+<TR>
+ <TD><EM>Microsoft, Windows, Windows 8.1, Windows Professional, являются зарегистрированными товарными знаками корпорации Майкрософт в США и других странах</EM></TD>
+ <TD ALIGN="right"><EM>&copy; 2023-2024 <A HREF="mailto:sementsul.maksim@yandex.ru">Sementsul Maxim</A></EM></TD>
+</TR>
+</TABLE>
+
+<center> <script type="text/javascript" src="//rf.revolvermaps.com/0/0/6.js?i=5e7ruf8bg56&amp;m=7&amp;c=e63100&amp;cr1=ffffff&amp;f=arial&amp;l=0&amp;bv=90&amp;lx=-420&amp;ly=420&amp;hi=20&amp;he=7&amp;hc=a8ddff&amp;rs=80" async="async"></script> </center>
+
+</BODY>
+
+<!-- Mirrored from pdp-11.org.ru/files.pl by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 24 Nov 2023 11:58:02 GMT -->
+</HTML>
+
